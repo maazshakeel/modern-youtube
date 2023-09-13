ba268ef96d8798dc47b139ec0b45fa06f63a43d9
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-13 02:30:19 -0400

    feat(app.js): add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Box } from "@mui/material"
+
+//..imports
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import ChannelDetail from './components/ChannelDetail'
+import VideoDetail from './components/VideoDetail'
+import SearchFeed from './components/SearchFeed'
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Box sx={{ backgroundColor: '#000' }}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact element={<Feed />} />
+                    <Route path="/video/:id" element={<VideoDetail />} />
+                    <Route path="/channel/:id" element={<ChannelDetail />} />
+                    <Route path="/search/:searchTerm" element={<SearchFeed />} />
+                </Routes>
+            </Box>
+        </BrowserRouter>
     )
 }
 
