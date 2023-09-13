1eb92208c402f38b2be13ab90daee7dabfa58876
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-13 02:36:04 -0400

    feat(index): index.js file to help other files to import components just directly by it

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// importing all of the components here and then app.js can easliy access all of them by importing them from here
+export { default as Navbar } from './Navbar'
+export { default as ChannelDetail } from './ChannelDetail'
+export { default as Feed } from './Feed'
+export { default as SearchFeed } from './SearchFeed'
+export { default as VideoDetail } from './VideoDetail'
