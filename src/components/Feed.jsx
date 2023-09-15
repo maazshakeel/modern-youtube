2214660f2dfc79b77de00a94003e346ff8882144
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-14 23:31:17 -0400

    feat(feed): add sidebar working (copyright things...)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 function Feed() {
   return (
-    <div>Feed</div>
-  )
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Muhammad Maaz
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
-export default Feed
+export default Feed;
