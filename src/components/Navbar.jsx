3b9ad137fa09b8048c7d6186bb8d2219cee0f8b8
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-14 22:55:37 -0400

    feat(navbar): search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,12 @@ function Navbar() {
   return (
     <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: "#000", top: 0, justifyContent: 'space-between' }}>
 
+      {/* Logo */}
       <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
         <img src={logo} alt="logo" height={45} />
       </Link>
 
+      {/* Search Bar in navbar */}
       <SearchBar />
     </Stack>
   )
