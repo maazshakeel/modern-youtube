7b2dc955ec5faadae6ebfe743e0e180b06951776
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-15 00:08:49 -0400

    feat(add sidebar in index): side bar in index to be imported

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 // importing all of the components here and then app.js can easliy access all of them by importing them from here
-export { default as Navbar } from './Navbar'
-export { default as ChannelDetail } from './ChannelDetail'
-export { default as Feed } from './Feed'
-export { default as SearchFeed } from './SearchFeed'
-export { default as VideoDetail } from './VideoDetail'
-export { default as SearchBar } from './SearchBar'
+export { default as Navbar } from "./Navbar";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as Feed } from "./Feed";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
